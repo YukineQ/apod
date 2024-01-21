6f07a473f081142737780442dad6b64b2787067d
YukineQ <mayberainq@gmail.com>
2024-01-22 01:53:29 +0300

    bring export back

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
     images: {
         domains: ['apod.nasa.gov', 'www.youtube.com'],
+        unoptimized: true,
     },
     env: {
         NASA_API_KEY: process.env.NASA_API_KEY
     },
+    output: 'export',
 }
 
 module.exports = nextConfig
